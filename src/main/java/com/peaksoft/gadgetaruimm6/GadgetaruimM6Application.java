0469e92159e3b265c0481422744573ffb5fddd50
medertynychbekov <meder.tynychbekov@gmail.com>
2023-09-18 18:13:52 +0300

    Initial commit

---
+++ src/main/java/com/peaksoft/gadgetaruimm6/GadgetaruimM6Application.java
@@ -0,0 +1,23 @@
+package com.peaksoft.gadgetaruimm6;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+public class GadgetaruimM6Application {
+
+    public static void main(String[] args) {
+        log.info("Hello from Gadgetaruim application");
+        SpringApplication.run(GadgetaruimM6Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String greetingPage() {
+        return "<h1>Welcome to Gadgetarium-M5 Application!!!<h1/>";
+    }
+}
---
+++ src/test/java/com/peaksoft/gadgetaruimm6/GadgetaruimM6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaksoft.gadgetaruimm6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetaruimM6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
