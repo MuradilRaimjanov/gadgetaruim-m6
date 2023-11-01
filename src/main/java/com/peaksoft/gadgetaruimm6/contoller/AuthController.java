2d4bc5d1402c36a69683170e025c642eb5227ca9
Админ <zulkumarteshebaeva@yandex.com>
2023-11-01 18:54:38 +0300

    add swagger documentation

--- src/main/java/com/peaksoft/gadgetaruimm6/contoller/AuthController.java
+++ src/main/java/com/peaksoft/gadgetaruimm6/contoller/AuthController.java
@@ -4,8 +4,6 @@ import com.peaksoft.gadgetaruimm6.model.dto.LoginRequest;
 import com.peaksoft.gadgetaruimm6.model.dto.RegisterRequest;
 import com.peaksoft.gadgetaruimm6.model.dto.RegisterResponse;
 import com.peaksoft.gadgetaruimm6.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,24 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-@Tag(name = "Authorization controller")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/sign-up")
-    @Operation(description = "This method  for registration ")
+    @PostMapping("/sing-up")
     public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest request) {
         return new ResponseEntity<>(authService.register(request), HttpStatus.OK);
     }
 
     @PostMapping("/sign-in")
-    @Operation(description = "This method is for authentication ")
-    public ResponseEntity<LoginResponse> signIn(@RequestBody LoginRequest request) {
-        if(request == null){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(authService.authentication(request),HttpStatus.OK);
     public ResponseEntity<?> signIn(@RequestBody LoginRequest request) {
         return new ResponseEntity<>(authService.authentication(request), HttpStatus.OK);
     }
