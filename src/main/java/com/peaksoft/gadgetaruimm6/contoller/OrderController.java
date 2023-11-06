2543ff76559ab56f5bf38e721d0cf8aba2c80200
Админ <zulkumarteshebaeva@yandex.com>
2023-11-06 21:41:08 +0300

    add Orders

--- src/main/java/com/peaksoft/gadgetaruimm6/contoller/OrderController.java
+++ src/main/java/com/peaksoft/gadgetaruimm6/contoller/OrderController.java
@@ -5,6 +5,9 @@ import com.peaksoft.gadgetaruimm6.model.dto.OrderResponse;
 import com.peaksoft.gadgetaruimm6.model.dto.ResponseToTheOrderForDelivery;
 import com.peaksoft.gadgetaruimm6.model.entity.Order;
 import com.peaksoft.gadgetaruimm6.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.Description;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +15,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/orders")
-//@Tag(name = "OrderController", description = "API endpoints for managing orders")
+@Tag(name = "OrderController")
 @RequiredArgsConstructor
 public class OrderController {
     private  final OrderService orderService;
 
     @PostMapping("save/{review-status}/{basketId}")
+    @Operation(description = "This method is created so that the user can place an order by the basket id")
     public ResponseToTheOrderForDelivery saveMain(@PathVariable("review-status") String orderReview ,
                                                   @PathVariable("basketId") Long basketId,
                                                   @RequestBody OrderRequest orderRequest) {
@@ -25,16 +29,19 @@ public class OrderController {
     }
 
     @PostMapping("save-payment/{id}/{review-status}")
+    @Operation(description ="With this method, the entire authenticated list of users can save the order payment")
     public OrderResponse savePayment(@PathVariable("id")Long id,@PathVariable ("review-status")
     String orderReview,@RequestBody OrderRequest orderRequest ){
         return orderService.savePayment(id,orderReview,orderRequest);
     }
 
     @PostMapping("save-finish/{id}/{review-status}")
+    @Operation(description ="Using this method, the User can save the total amount of basket items in an already created order by its identifier")
     public OrderResponse saveReview(@PathVariable("id") Long id, @PathVariable("review-status") String orderReview) {
         return orderService.saveOrderReview(id, orderReview);
     }
     @GetMapping("{id}")
+    @Operation(description ="This method find order by id")
     public Optional<Order> findOrderById(@PathVariable Long id) {
         return orderService.getOrderById(id);
     }
